e0200a94c1277946383c66df35b2646780d3a43d
unknown <viola4lfe@gmail.com>
2020-02-10 14:54:33 -0500

    setup working GET requests

--- index.js
+++ index.js
@@ -7,12 +7,39 @@ const server = express();
 
 server.use(express.json()); // needed for POST, PUT/PATCH
 
-server.get("/api/hubs", (req, res) => {
+// GET REQUESTS:
+
+server.get("/api/users", (req, res) => {
   // needed to get hubs from database (db.js)
   Hubs.find()
-    .then()
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
     .catch(error => {
-      console.log(err);
+      console.log(error);
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+      });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+ const {id} = req.params;
+  Hubs.findById(id)
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).json({
+        errorMessage: "The user with the specified ID does not exist."
+      })
+      .catch(err => {
+       console.log(err);
+       res.status(500).json({
+        errorMessage: "The user information could not be retrieved."
+       })
+      })
     });
 });
 
