4aa2c47099e5ae926b5c846b17c4185af3a9e2ff
unknown <viola4lfe@gmail.com>
2020-02-10 14:31:27 -0500

    set the server port to 5000, brought in database form db.js, sending get for list of hubs

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // implement your API here
 const express = require("express");
 
+const Hubs = require("./data/db.js"); // need this line to get the list of hubs
+
 const server = express();
+
+server.use(express.json()); // needed for POST, PUT/PATCH
+
+server.get("/api/hubs", (req, res) => {
+  // needed to get hubs from database (db.js)
+  Hubs.find()
+    .then()
+    .catch(error => {
+      console.log(err);
+    });
+});
+
+const port = 5000;
+server.listen(port, () => {
+  console.log(`\n** API on port ${port} \n`);
+});
